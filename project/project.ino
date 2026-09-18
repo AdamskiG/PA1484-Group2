2d6c821aef04b0cac06891c492ddaaef1f541aa3
Adam Gunnarsson <44670986+AdamskiG@users.noreply.github.com>
2026-09-18 16:33:47 +0200

    Cleaned up project.ino
    Removed comments from testing and reset the tile 1's text to something more neutral.

--- project/project.ino
+++ project/project.ino
@@ -52,7 +52,7 @@ static void create_ui()
   // Tile #1
   {
     t1_label = lv_label_create(t1);
-    lv_label_set_text(t1_label, "Adam is Handsome, he's the handsommest of them all and now we're out of bounds*");
+    lv_label_set_text(t1_label, "Main screen");
     lv_obj_set_style_text_font(t1_label, &lv_font_montserrat_28, 0);
     lv_obj_center(t1_label);
     apply_tile_colors(t1, t1_label, /*dark=*/false);
@@ -70,11 +70,6 @@ static void create_ui()
     lv_obj_add_event_cb(t2, on_tile2_clicked, LV_EVENT_CLICKED, NULL);
   }
 
-  //Här skriver jag en kommentar som jag ska följa. -Adam
-  //Test 2
-
-  //Test 4
-  //TEst 5 is here
 }
 
 // Function: Connects to WIFI
