b219a78ebe84ec0c340394b1f5115926b5233fa2
Eiferuz <napat_626@hotmail.com>
2026-09-22 15:51:29 +0200

    Update project.ino

--- project/project.ino
+++ project/project.ino
@@ -52,7 +52,7 @@ static void create_ui()
   // Tile #1
   {
     t1_label = lv_label_create(t1);
-    lv_label_set_text(t1_label, "Main screen");
+    lv_label_set_text(t1_label, "Public Transport Information and Interaction on ESP32\n Group 2 Version 1.0 \n Napatpong Chokwatthananon \n Li Holma, Tim Säterby \n Adam Gunnarsson, Kimi Grubisic");
     lv_obj_set_style_text_font(t1_label, &lv_font_montserrat_28, 0);
     lv_obj_center(t1_label);
     apply_tile_colors(t1, t1_label, /*dark=*/false);
